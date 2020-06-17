9e96ea30636cdbd489601b641460b4e7b4f554a4
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-06-16 17:23:32 -0700

    6/16 successfully generates access_token on artist submission to access API

--- app.js
+++ app.js
@@ -1,55 +1,31 @@
-
-const display = () => {
-    $('img').remove();
-    $('ul').empty();
-    $('#a1Header').text($('#first').val());
-    $('#a2Header').text($('#second').val());
-    $('#a3Header').text($('#third').val());
-    getInfo(($('#first').val()),$('#a1List'),$('#a1'));
-    getInfo(($('#second').val()),$('#a2List'),$('#a2'));
-    getInfo(($('#third').val()),$('#a3List'),$('#a3'));
-    $('input').val("");
-    $('h3').text("classified as: ")
+function getUserAccessToken(client_64) {
+    const resp = $.ajax({
+        url: "https://accounts.spotify.com/api/token",
+        method: 'POST',
+        data: "grant_type=client_credentials",
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("Authorization", `Basic ${client_64}`);
+        },
+    })
+    return (resp);
 }
 
-// use seed artists, check if there are any common genres between, if so, use as 4th and or 5th seed; set limit as 5
-// use a slider for tuneable track attributes? ex popularity, energy, acousticness
-// figure out how to add web player?
-
-const getInfo = (artist,uList,section) => {
-
-    const userAccessToken = "BQArclFEu9MQgXEujIkmK4sHF_asGXCZ8QdlgLKNSw6zKzFMyNkZR9tvkWyfJ52QnR8qBgTP10nUtUScwo2OlNH9vmsFkyIY_m3lsM5KOBAP4-RGBB7ARSPy478xp4PfT4kJU8mNQ6PA2C4";
-
-    const client_id = "2483a99e8ce64d8fa66813fae67f3610";
-    const client_secret = "8b0d5cb1f93841258b4bc00e100eec8f";
-    const redirect_uri = "https%3A%2F%2Froot2point0.github.io%2Fspot.me";
-    const scopes = "user-read-private user-read-email";
-
-
-    // let userAccessToken = null;
-    // $.ajax ({
-    //     url: `https://accounts.spotify.com/authorize?client_id=${client_id}&scopes=${scopes}&response_type=code&redirect_uri=${redirect_uri}`,
-    //     success: function (data) {
-    //         userAccessToken = data.code;
-    //     }
-    // })
-
+const getIdInfo = (userAccessToken,artist,uList,section) => {
     artist = artist.replace(/ /g,"%20");
-    console.log(artist);
+    // get artist id for user input
     $.ajax({
         url: `https://api.spotify.com/v1/search?q='${artist}'&type=artist`,
-        beforeSend: function (xhr) {
+        beforeSend: (xhr) => {
             xhr.setRequestHeader("Authorization", `Bearer ${userAccessToken}`)
-        }, success: function(data) {
-            artistId = data.artists.items[0].id;
+        }, success: (data) => {
+            const artistId = data.artists.items[0].id;
     
-            // nested ajax call?
+            // use id to get artist data
             $.ajax({
                 url: `https://api.spotify.com/v1/artists/${artistId}`,
                 beforeSend: function (xhr) {
                     xhr.setRequestHeader("Authorization", `Bearer ${userAccessToken}`)
-                }, success: function(data) {
-                    console.log(data);
+                }, success: (data) => {
                     for (let genre of data.genres) {
                         const $li = $('<li>').text(genre);
                         (uList).append($li);
@@ -60,4 +36,22 @@ const getInfo = (artist,uList,section) => {
             })
         }  
     })
+}
+
+
+const display = () => {
+    const client_64 = "MjQ4M2E5OWU4Y2U2NGQ4ZmE2NjgxM2ZhZTY3ZjM2MTA6OGIwZDVjYjFmOTM4NDEyNThiNGJjMDBlMTAwZWVjOGY=";
+    getUserAccessToken(client_64).then((response) => {   
+        const userAccessToken = response.access_token; 
+        $('img').remove();
+        $('ul').empty();
+        $('#a1Header').text($('#first').val());
+        $('#a2Header').text($('#second').val());
+        $('#a3Header').text($('#third').val());
+        getIdInfo(userAccessToken,($('#first').val()),$('#a1List'),$('#a1'));
+        getIdInfo(userAccessToken,($('#second').val()),$('#a2List'),$('#a2'));
+        getIdInfo(userAccessToken,($('#third').val()),$('#a3List'),$('#a3'));
+        $('input').val("");
+        $('h3').text("classified as: ")
+    });
 }
